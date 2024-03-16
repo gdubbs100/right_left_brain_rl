9fdd8e6ae43d11a0d48594aa692fff708ff18b3d
unknown <grantwn1988@gmail.com>
2024-03-17 09:48:17 +1100

    run scenarios script fiddle

--- run_scenarios.py
+++ run_scenarios.py
@@ -6,20 +6,20 @@ SCRIPT = './run_continual_learner.py'
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--algorithm', type = str, default = 'random', help = 'Give a name to your run for ease of logging')
-parser.add_argument('--steps_per_env', type=str, default='100', help = 'number of iters')
+parser.add_argument('--steps_per_env', type=str, default='1000000', help = 'number of iters')
 parser.add_argument('--log_folder', type=str, default='logs/baselines/random_agent', help = 'where to save results')
-parser.add_argument('--randomization', type=str, default='random_init_all', help = 'whether env randomises on reset')
+parser.add_argument('--randomization', type=str, default='random_init_fixed20', help = 'whether env randomises on reset')
 parser.add_argument('--run_folder', type=str, default='rl2_baseline/bicameral_rl2')
 parser.add_argument('--num_processes', type=str, default='8')
 
 args, _ = parser.parse_known_args()
 envs = [
-    # 'pick-place-v2',
-    # 'reach-v2',
-    # 'push-v2',
-    # 'reach-wall-v2',
-    # 'push-wall-v2',
-    # 'pick-place-wall-v2',
+    'pick-place-v2',
+    'reach-v2',
+    'push-v2',
+    'reach-wall-v2',
+    'push-wall-v2',
+    'bin-picking-v2',
     'door-open-v2', 
     'button-press-v2', 
     'faucet-open-v2'
